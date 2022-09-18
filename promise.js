39447d962c92ecafa15f45e188bede445c1e6c37
aseprizal <aseprizal70@gmail.com>
2022-09-18 18:48:00 +0700

    submit1

--- promise.js
+++ promise.js
@@ -1,7 +1,53 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+// const promiseOutput = null;
+
+const promiseOutput = (opsi) => {
+  // Hasil akhir dari fungsi ini adalah dengan
+  // mengembalikan Promise
+  return new Promise((resolve, reject) => {
+    // Kondisi gagal
+    if (opsi=== null) {
+      reject('Maaf, opsi Error !');
+    }
+    // Kondisi terpenuhi
+    else {
+      resolve(opsi);
+        (async () => {
+          var hasilx = await promiseTheaterIXX(); 
+          var hasilc = await promiseTheaterVGC(); 
+          counterm = 0
+          countertm = 0
+          for (tmphasilx of hasilx) {
+            if (tmphasilx['hasil'] == opsi && opsi == 'tidak marah') {
+              // console.log(tmphasilx['namaFilm']+ ' - ' +tmphasilx['hasil'])
+              countertm++;
+            }else if (tmphasilx['hasil'] == opsi && opsi == 'marah') {
+              // console.log(tmphasilx['namaFilm']+ ' - ' +tmphasilx['hasil'])
+              counterm++;
+            }
+            
+          };
+          for (tmphasilc of hasilc) {
+            if (tmphasilc['hasil'] == opsi && opsi == 'tidak marah') {
+              // console.log(tmphasilc['namaFilm']+ ' - ' +tmphasilc['hasil'])
+              countertm++;
+            }
+            else if (tmphasilc['hasil'] == opsi && opsi == 'marah') {
+              // console.log(tmphasilc['namaFilm']+ ' - ' +tmphasilc['hasil'])
+              counterm++;
+            }
+          };
+          if(opsi == 'marah'){
+            console.log(counterm);
+          }else if (opsi == 'tidak marah'){
+            console.log(countertm);
+          }
+        })();
+    }
+  });
+};
 
 module.exports = {
   promiseOutput,
